3f085142612f44c355ffdd4b787ff43bbb25a977
Skizor <dontgodowntosorrow@bk.ru>
2018-11-26 14:13:02 +0600

    ready backend php

---
+++ src/config/db.php
@@ -0,0 +1,19 @@
+<?php
+	class db{
+		//Poroperties
+		private $dbhost = 'localhost';
+		private $dbuser = 'admin';
+		private $dbpass = 'admin';
+		private $dbname = 'eucliwood';
+
+		//Connect
+		public function connect(){
+			$mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
+			$dbConnection = new PDO($mysql_connect_str, $this->dbuser, $this->dbpass);
+			$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return $dbConnection;
+		}
+	}
+
+
+?>
